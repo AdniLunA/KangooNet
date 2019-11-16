0708e0c855c23ac8bf3bd91747f783dcfef538ff
Benjamin Wittke <wittke.benjamin@web.de>
2019-11-16 13:56:48 +0100

    mongodb connect

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+
+const dbUrl = 'mongodb+srv://Herta:PKC5ZLoLNcg2zEez@kangoonet-entqn.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = "KangooNet";
+
+
+MongoClient.connect(dbUrl, function(err, client) {
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+
+    client.close();
+});
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
@@ -9,6 +22,11 @@ app.get('/200', function (req, res) {
     res.send('jo!');
 })
 
+app.get('/conference', function (req, res) {
+    res.send('jo!');
+})
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
