f139511043132faf1f8eb7cd7ae835fa615050b2
boppli <linda.bopp@mail.schwarz>
2019-11-17 12:26:44 +0100

    add user mapping

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,52 +1,89 @@
 import {Injectable} from '@angular/core';
 import {ApiService} from './api.service';
-import {IUser} from '../interfaces/user';
+import {IUser, RoleType} from '../interfaces/user';
+import {ITag, TagType} from "../interfaces/tag";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class UserService {
-  constructor(private apiService: ApiService) {}
+    constructor(private apiService: ApiService) {
+    }
 
-  getUserList() {
-    return this.apiService.get('/user');
-  }
+    getUserList() {
+        return this.apiService.get('/user');
+    }
 
-  getUser(id: string) {
-    //return user from api
-    return this.apiService.get('/user/' + id);
-  }
+    getUser(id: string) {
+        //return user from api
+        return this.apiService.get('/user/' + id);
+    }
 
-  getUserByName(name: string) {
-    return this.apiService.get('/login/' + name);
-  }
+    getUserByName(name: string) {
+        return this.apiService.get('/login/' + name);
+    }
 
-  getAvatarList() {
-    return this.apiService.get('/getAvatar');
-  }
+    getAvatarList() {
+        return this.apiService.get('/getAvatar');
+    }
 
-  createUser(name: string, avatar: string, role: string) {
-    //return user from api
-    return this.apiService.post('/create', {
-      name,
-      avatar,
-      role,
-    });
-  }
+    createUser(name: string, avatar: string, role: string) {
+        //return user from api
+        return this.apiService.post('/create', {
+            name,
+            avatar,
+            role,
+        });
+    }
 
-  addContact(idContact: string, idUser: string) {
-    return this.apiService.put('/user/add/' + idContact + '/asFirendTo/' + idUser, null);
-  }
+    addContact(idContact: string, idUser: string) {
+        return this.apiService.put('/user/add/' + idContact + '/asFirendTo/' + idUser, null);
+    }
 
-  addJob(id: string, req: string[], name: string) {
-    return this.apiService.put('/addJob/' + id, {
-      name,
-      req,
-    });
-  }
+    addJob(id: string, req: string[], name: string) {
+        return this.apiService.put('/addJob/' + id, {
+            name,
+            req,
+        });
+    }
 
-  transformUser(response: any): IUser {
-    // @todo
-    return null;
-  }
+    transformUser(response: any): IUser {
+        const userRes = response.user;
+        return {
+            id: response._id,
+            role: userRes.role as RoleType,
+            name: userRes.name,
+            avatar: userRes.avatar,
+            linkList: [],
+            jobList: [],
+            tagList: {
+                skillList: [],
+                interestList: [],
+                locationList: [],
+            },
+            contactList: [],
+            attendance: {
+                conference: {
+                    id: userRes.attendance.conference.id,
+                    name: userRes.attendance.conference.name,
+                    logo: userRes.attendance.conference.logo,
+                    tagList: {
+                        location: this.convertToTag(userRes.attendance.conference.tagList.location, 'location'),
+                        skillList: userRes.attendance.conference.tagList.skillList.map(item => this.convertToTag(item, 'skill'))
+                    },
+                    expirationDate: new Date(userRes.attendance.conference.expirationDate),
+                },
+            },
+            upcomingConferenceList: [],
+            conferenceHistory: [],
+        }
+    }
+
+    convertToTag(item: string, type: TagType): ITag {
+        return {
+            name: item,
+            type,
+            allowDelete: true,
+        };
+    }
 }
